f4914ede3abee946cb52fd93f5edb41542afbcca
Ángel Cámara <a.camarare@alumnos.urjc.es>
2017-03-03 21:19:05 +0100

    Terminado

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
 # X-Nav-Modernizr
+
+## Enunciado
+Bájate e instálate la biblioteca de Javascript Modernizr, que se utiliza para comprobar si la funcionalidad de HTML5 está soportada por el navegador (prueba con Firefox y con Konqueror, el navegador de KDE últimamente con menos desarrollo). Comprueba si el navegador tiene la siguiente funcionalidad:
+-  canvas
+-  video
+-  video en formato ogg
+-  almacenamiento local
+-  apliaciones sin conexión
+-  geolocalización
+
+## Resultado
+
+[https://acamara7es.github.io/X-Nav-Modernizr](https://acamara7es.github.io/X-Nav-Modernizr/index.html)
---
+++ script.js
@@ -0,0 +1,43 @@
+var yes = "<span class='supported'>YES</span> "
+var no = "<span class='supported'>NO</span> "
+
+function adder(id, plusValue) {
+	var element = document.getElementById(id);
+	element.innerHTML = plusValue + element.innerHTML;
+}
+
+if (Modernizr.canvas) {
+	adder("canvas", yes);
+} else {
+	adder("canvas", no);
+}
+
+if (Modernizr.video) {
+	adder("video", yes);
+} else {
+	adder("video", no);
+}
+
+if (Modernizr.video.ogg) {
+	adder("ogg", yes);
+} else {
+	adder("ogg", no);
+}
+
+if (Modernizr.localstorage) {
+	adder("local", yes);
+} else {
+	adder("local", no);
+}
+
+if (Modernizr.applicationcache) {
+	adder("offline", yes);
+} else {
+	adder("offline", no);
+}
+
+if (Modernizr.geolocation) {
+	adder("geo", yes);
+} else {
+	adder("geo", no);
+}
